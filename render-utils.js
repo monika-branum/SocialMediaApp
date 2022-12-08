617a3ff15ba91656cbacf5358823a206cefaa57d
spiderforrest <spider@spood.org>
2022-12-08 12:54:54 -0800

    some basic css, reformmated images, working message date display

--- render-utils.js
+++ render-utils.js
@@ -3,34 +3,38 @@ import { decrementScore, incrementScore } from './fetch-utils.js';
 export function renderUserList(data) {
     // create
     const div = document.createElement('div');
-    const img = document.createElement('img');
+    const imgDiv = document.createElement('div');
     const a = document.createElement('a');
     const p = document.createElement('p');
     // populate
-    img.src = data.avatar_url;
+    imgDiv.style.backgroundImage = `url('${data.avatar_url}')`;
     a.textContent = data.username;
     a.href = `./detail/?id=${data.id}`;
     p.textContent = '⭐' + data.score;
-
+    // style
+    imgDiv.classList.add('img');
+    a.classList.add('username-link');
     // consolidate
-    div.append(img, a, p);
+    div.append(imgDiv, a, p);
     return div;
 }
 
 export function renderUserDetails(data) {
     // create
     const div = document.createElement('div');
-    const img = document.createElement('img');
+    const imgDiv = document.createElement('div');
     const h = document.createElement('h2');
     const bioP = document.createElement('p');
     const scoreP = document.createElement('p');
     // populate
-    img.src = data.avatar_url;
+    imgDiv.style.backgroundImage = `url('${data.avatar_url}')`;
     h.textContent = data.username;
     bioP.textContent = data.bio;
     scoreP.textContent = '⭐: ' + data.score;
+    // style
+    imgDiv.classList.add('img');
     // consolidate
-    div.append(img, h, bioP, scoreP);
+    div.append(imgDiv, h, bioP, scoreP);
     return div;
 }
 
@@ -38,12 +42,20 @@ export function renderMessage(data) {
     // create
     const messageDiv = document.createElement('div');
     const fromH = document.createElement('h3');
+    const timeH = document.createElement('h6');
     const bodyP = document.createElement('p');
     // populate
     fromH.textContent = data.from_email || 'something has gone terribly wrong: sender not found';
+    timeH.textContent = new Date(data.created_at).toLocaleString('en-US', {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    });
     bodyP.textContent = data.text;
     // consolidate
-    messageDiv.append(fromH, bodyP);
+    messageDiv.append(fromH, timeH, bodyP);
     return messageDiv;
 }
 
